43de276655f17e0b128dd414726862c3bf14ea8c
Ohinoyi <ohimoiza12@gmail.com>
2023-05-11 13:01:13 +0100

    Create amenity.py

---
+++ clone/models/amenity.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module begins creating an Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """This is the class for managing all amenity objects"""
+
+    name = ""
